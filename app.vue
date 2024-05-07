3f76aa1bcb8fe39f9ab94f55a63a77a185f0d6da
rakibmiah99 <rakibmiah120199@gmail.com>
2024-05-07 23:27:55 +0600

    under constaction

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtPage/>
+  <div class="h-[100vh] w-full flex justify-center items-center">
+    <h1 class="text-4xl font-bold">This Site Under Construction</h1>
+    <!-- <NuxtPage/> -->
   </div>
 </template>
